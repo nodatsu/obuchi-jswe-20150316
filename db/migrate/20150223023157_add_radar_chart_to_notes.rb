82a6e1d5e9faea7714aa91a5a5699dc29364b206
oosawa ryouhei <si12005@edu.aomori-u.ac.jp>
2015-03-12 12:27:58 +0900

    string

--- db/migrate/20150223023157_add_radar_chart_to_notes.rb
+++ db/migrate/20150223023157_add_radar_chart_to_notes.rb
@@ -1,5 +1,5 @@
 class AddRadarChartToNotes < ActiveRecord::Migration
   def change
-    add_column :notes, :radarchart_file_name, :strings
+    add_column :notes, :radarchart_file_name, :string
   end
 end
